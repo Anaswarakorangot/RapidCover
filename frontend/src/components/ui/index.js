032e189b15eb5eb898efe7ef7126466cf1a71411
Yuvahasini <159130294+Yuvahasini@users.noreply.github.com>
2026-04-01 15:35:45 +0530

    Updated service worker, UI index and notification context

--- frontend/src/components/ui/index.js
+++ frontend/src/components/ui/index.js
@@ -1,3 +1,5 @@
-export { Button } from './Button';
-export { Card, CardHeader, CardBody, CardFooter } from './Card';
-export { Input, OTPInput } from './Input';
+export { Button } from "./Button";
+export { Card, CardHeader, CardBody, CardFooter } from "./Card";
+export { Input, OTPInput } from "./Input";
+export { CoverageCard } from "./CoverageCard";
+export { ClaimList, TRIGGER_ICONS, TRIGGER_LABELS } from "./ClaimList";
