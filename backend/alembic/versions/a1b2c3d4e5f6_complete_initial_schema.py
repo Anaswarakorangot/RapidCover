924bf0826dd12066a233c566f88701452fb553f0
A V K Bhavan Surya <avkbsurya@gmail.com>
2026-04-17 11:15:00 +0530

    fix(db): Chain migrations from old Render revision to new schema

--- backend/alembic/versions/a1b2c3d4e5f6_complete_initial_schema.py
+++ backend/alembic/versions/a1b2c3d4e5f6_complete_initial_schema.py
@@ -1,7 +1,7 @@
 """Complete initial schema
 
 Revision ID: a1b2c3d4e5f6
-Revises:
+Revises: 8a1b2c3d4e5f
 Create Date: 2026-04-18 18:00:00.000000
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = None
+down_revision: Union[str, Sequence[str], None] = '8a1b2c3d4e5f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
